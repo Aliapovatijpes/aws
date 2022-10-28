a9789693dd5bf1d5e1a4e53979a7dd588df4b6d8
Aliapovatijpes <84346624+Aliapovatijpes@users.noreply.github.com>
2022-10-28 13:00:48 +0300

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,6 @@
+dictionary = {}
+dictionary [1] = ["hellow"]
+dictionary [2] = ["world"]
+print (dictionary)
+for asd, dsa in dictionary.items():
+    print(asd, dsa)
---
+++ queue.py
@@ -0,0 +1,8 @@
+line=["ann","Pan","Alisa", "july", "anastasiia"]
+print (line)
+line.insert(0,"new wmen1")
+print (line)
+line.pop(3)
+print(line)
+line.append("last women")
+print(line)
